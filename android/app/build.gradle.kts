69e90ad3091dfa900cac6fb17c79b82a2cfdf963
Martin Jäger <martin@libre.solar>
2025-06-27 10:12:53 +0200

    Re-create all platforms with flutter create to fix Android issues
    Android app did not compile properly anymore because of NDK and gradle
    compatibility issues.
    
    By re-creating the platforms using `flutter create` and copying back
    previously made changes, the project should now be like new again.
    
    Android SDK and NDK versions are now fixed in build.gradle.kts, instead
    of importing the version numbers from local.properties file. This
    should make local builds and builds in CI more reproducible.

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,63 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+android {
+    namespace = "io.thingset.client"
+    compileSdk = 35
+    ndkVersion = "29.0.13599879"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        // Unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "io.thingset.client"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = 24
+        targetSdk = 35
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
+        }
+        debug {
+            applicationIdSuffix = ".debug"
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
